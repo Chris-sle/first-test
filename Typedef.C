1efc3235908ab772813d8d68c2b09eba68890bcb
Chris <oscar.sletvold@gmail.com>
2026-01-05 11:32:15 +0100

    Update Typedef.C

--- Typedef.C
+++ Typedef.C
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main() {
+    // Typedef in C/C++ is a reservered keyword that gives an existing datatype a "nickname" or alias.
+    // It allows you to create a new name for an existing data type, making the code more readable and easier to maintain.
+    // For example, instead of writing 'int' everywhere, you can define a new type like 'Integer'.
+
     // Example of typedef usage
     typedef int Integer; // Defining a new type 'Integer' as an alias for 'int'
     
